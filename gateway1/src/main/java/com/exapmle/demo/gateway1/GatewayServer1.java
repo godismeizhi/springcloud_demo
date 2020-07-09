7247d640be68338b6ec27d05fe2d65047d10ff0c
godsimeizhi <godsimeizhi@mail.com>
2020-07-09 16:08:08 +0800

    修改

--- gateway1/src/main/java/com/exapmle/demo/gateway1/GatewayServer1.java
+++ gateway1/src/main/java/com/exapmle/demo/gateway1/GatewayServer1.java
@@ -1,11 +1,13 @@
+package com.exapmle.demo.gateway1;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class GatewayServer {
+public class GatewayServer1 {
     public static void main(String[] args) {
-        SpringApplication.run(GatewayServer.class, args);
+        SpringApplication.run(GatewayServer1.class, args);
     }
 }
